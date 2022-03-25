ba4ba7166e9c4a464c029e011f5fad0b1cf5f5a5
dev-shahid <shahidul.cse46@gmail.com>
2022-03-25 22:58:30 +0600

    working with pagination

--- src/components/Authors/Authors.js
+++ src/components/Authors/Authors.js
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import ListItemComponent from '../ListItemComponent/ListItemComponent';
 import { Pagination } from 'antd';
 import 'antd/dist/antd.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ListItemComponent from '../ListItemComponent/ListItemComponent';
+import styles from './Authors.module.css';
 
 function Authors(props) {
     const [authors, setAuthors] = useState([]);
     const [page, setPage] = useState(1);
     const [totalPage, setTotalPage] = useState(1);
+    const [totalItems, setTotalItems] = useState(1);
 
     useEffect(() => {
         (async () => {
@@ -15,6 +17,7 @@ function Authors(props) {
             console.log('res...................', res?.data);
             setTotalPage(res?.data?.totalPages);
             setAuthors(res?.data?.results);
+            setTotalItems(res?.data?.totalCount);
         })()
     }, [page])
 
@@ -22,13 +25,16 @@ function Authors(props) {
 
     return (
         <div>
-            {
-                authors?.map((author) => (
-                    <ListItemComponent author={author} />
-                ))
-            }
-            <div style={{textAlign: 'center'}}>
-                <Pagination hideOnSinglePage responsive current={page} total={totalPage} onChange={(page) => setPage(page)} />
+            <h2 style={{textAlign: 'center', margin: '20px 0px'}}>Authors</h2>
+            <div  className={styles.authors__container}>
+                {
+                    authors?.map((author) => (
+                        <ListItemComponent author={author} />
+                    ))
+                }
+            </div>
+            <div style={{textAlign: 'center', marginBottom: '30px', marginTop: '20px'}}>
+                <Pagination current={page} total={totalItems} pageSize={authors?.length} onChange={(page) => setPage(page)} />
             </div>
         </div>
     );
--- src/components/ListItemComponent/ListItemComponent.js
+++ src/components/ListItemComponent/ListItemComponent.js
@@ -3,8 +3,9 @@ import styles from './ListItemComponent.module.css';
 function ListItemComponent({author}) {
     console.log('props.............', author);
     return (
-        <div>
-            {author?.name}
+        <div className={styles.list__container}>
+            <p>Name: {author?.name}</p>
+            <p>Bio: {author?.bio}</p>
         </div>
     );
 }
